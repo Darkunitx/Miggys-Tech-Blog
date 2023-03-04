4102ac4ee27faf3510f2a4a764847f1a36ce345a
Darkunitx <darkunitx@live.com>
2023-03-04 16:55:47 -0600

    subcateorized models

--- models/index.js
+++ models/index.js
@@ -2,4 +2,35 @@ const Users = require('./user');
 const Posts = require('./post');
 const Comments = require('./comment');
 
+Users.hasMany(Posts, {
+    foreignKey: 'user_id'
+});
+
+Posts.belongsTo(Users, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Users.hasMany(Comments, {
+    foreignKey: 'user_id'
+});
+
+Comments.belongsTo(Users, {
+    foreignKey: 'user_id',
+});
+
+Posts.hasMany(Comments, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+// Comments.hasOne(Posts, {
+//     foreignKey: 'post_id'
+// });
+
+Comments.belongsTo(Posts, {
+    foreignKey: 'post_id',
+});
+
+
 module.exports = { Users, Posts, Comments };
